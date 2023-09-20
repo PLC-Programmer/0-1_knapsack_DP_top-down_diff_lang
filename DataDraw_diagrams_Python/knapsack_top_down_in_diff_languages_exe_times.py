17c5ab6a07559a00e3c27eaf37f9a47e6b01c32a
R.Sackmann <iec61131@my.mail.de>
2023-09-20 14:09:43 +0200

    Update knapsack_top_down_in_diff_languages_exe_times.py

--- DataDraw_diagrams_Python/knapsack_top_down_in_diff_languages_exe_times.py
+++ DataDraw_diagrams_Python/knapsack_top_down_in_diff_languages_exe_times.py
@@ -5,7 +5,7 @@ algorithm for different test cases and coded in different programming languages
 using the DataDraw Python library
 """
 #
-# Robert Sackmann, 2023-09-20
+# Robert Sackmann, 2023-09-20a
 #
 #
 # sources:
@@ -40,25 +40,29 @@ def run_all():
     svgset1 = {}
 
     # using method for an individual language:
-    svgset1['python']  = bar_diagram.lang(import_str='exe_times_01_python', \
-                                          exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
-    svgset1['cpp']  = bar_diagram.lang(import_str='exe_times_02_cpp', \
-                                       exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
-    svgset1['cs']  = bar_diagram.lang(import_str='exe_times_03_cs', \
-                                      exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
-    svgset1['rust']  = bar_diagram.lang(import_str='exe_times_04_rust', \
-                                      exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
+    # svgset1['python']  = bar_diagram.lang(import_str='exe_times_01_python', \
+    #                                       exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
+    # svgset1['cpp']  = bar_diagram.lang(import_str='exe_times_02_cpp', \
+    #                                    exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
+    # svgset1['cs']  = bar_diagram.lang(import_str='exe_times_03_cs', \
+    #                                   exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
+    # svgset1['rust']  = bar_diagram.lang(import_str='exe_times_04_rust', \
+    #                                   exclude='03_WEIGHTS100_Xu_Xu_et_al.in')
 
     # using method for a group of languages for both: picks on + off:
-    svgset1['lang1_WEIGHTS100_Xu_Xu'] = \
-       bar_diagram.lang_grp(import_str='exe_times_WEIGHTS100_Xu_Xu_lang1', exclude='none')
+    # svgset1['lang1_WEIGHTS100_Xu_Xu'] = \
+    #    bar_diagram.lang_grp(import_str='exe_times_WEIGHTS100_Xu_Xu_lang1', exclude='none')
 
     # filtering out Python, then call:
-    svgset1['lang1_WEIGHTS_TODD_18'] = \
-      bar_diagram.lang_grp(import_str='exe_times_WEIGHTS_TODD_18_lang1', exclude='Python')
-    svgset1['lang1_WEIGHTS24_Kreher_Stinson'] = \
-       bar_diagram.lang_grp(import_str='exe_times_WEIGHTS24_Kreher_Stinson_lang1', exclude='Python')
-   
+    # svgset1['lang1_WEIGHTS_TODD_18'] = \
+    #   bar_diagram.lang_grp(import_str='exe_times_WEIGHTS_TODD_18_lang1', exclude='Python')
+    # svgset1['lang1_WEIGHTS24_Kreher_Stinson'] = \
+    #   bar_diagram.lang_grp(import_str='exe_times_WEIGHTS24_Kreher_Stinson_lang1', exclude='Python')
+
+    # C++: effect of different g++ optimization options:
+    svgset1['cpp_gpp_WEIGHTS100_Xu_Xu'] = \
+        bar_diagram.cpp_gpp(import_str='exe_times_02_cpp_gpp_options_WEIGHTS100_Xu_Xu')
+
     return svgset1
 
 
@@ -71,7 +75,10 @@ class BarsDiagram:
 
 
     def lang(self, import_str='exe_times_01_python', exclude='none'):
-        """ ...  """
+        """
+        - diagrams for one programming language and different test cases
+        - picks on + picks off in one diagram
+        """
         d_d = self.d_d
 
         chunks = re.split('_', import_str)
@@ -151,7 +158,6 @@ class BarsDiagram:
                     test_nan_picks_off.append(0)
 
 
-
         y_max = max(*test_values_picks_on, *test_values_picks_off)
         # print("  y_max =", y_max)
 
@@ -203,9 +209,9 @@ class BarsDiagram:
 
     def lang_grp(self, import_str='exe_times_WEIGHTS100_Xu_Xu_lang1', exclude = 'none'):
         """
-        - picks on + picks off in one diagram:
-        - this is for test cases:
-          03_WEIGHTS100_Xu_Xu_et_al.in
+        - diagrams for different programming languages and one test case
+          - this is for test case 03_WEIGHTS100_Xu_Xu_et_al.in
+        - picks on + picks off in one diagram
         """
         d_d = self.d_d
 
@@ -323,6 +329,67 @@ class BarsDiagram:
                     nan_label += "\n  " + test_cases_picks_off[i]
                 i += 1
             d_d.label(4, y_max*0.97, nan_label)  # 4 = test case #4 as x-position
+
+        return d_d.svgresult()
+
+
+
+    def cpp_gpp(self, import_str='exe_times_02_cpp_gpp_options_WEIGHTS100_Xu_Xu'):
+        """
+        - diagram for C++: effect of different g++ optimization options
+        (- picks on diagram)
+        """
+        d_d = self.d_d
+
+        diagram_title = 'C++: g++ optimization options, test case: 03_WEIGHTS100_Xu_Xu_et_al.in'
+
+        import_str1 = 'import ' + import_str
+        # print("import_str1 =", import_str1)
+        import_str2 = "exec(" + '"' + import_str1 + '")'
+        # print("import_str2 =", import_str2)
+
+        # target:
+        # eval('exec("import exe_times_02_cpp_gpp_options_WEIGHTS100_Xu_Xu")')
+        eval(import_str2)
+
+        import_str3 = import_str + '.picks_on'
+        mydata_picks_on = eval(import_str3)
+        print("   mydata_picks_on =",  mydata_picks_on)
+        print("   len(mydata_picks_on) =",  len(mydata_picks_on))
+
+        d_d.svgbegin(600, 550)
+        d_d.settext(ptsize=14, color='#777')
+        d_d.setline(color='#aaa', save=True)
+
+        test_cases_picks_on = []
+        test_values_picks_on = []
+
+
+        for i, item in enumerate(mydata_picks_on):
+            d_d_tuple = item
+            test_case_str = d_d_tuple[0] + ', picks on'
+            test_cases_picks_on.append(test_case_str)
+            test_values_picks_on.append(d_d_tuple[1])
+
+
+        y_max = max(*test_values_picks_on)
+        print("  y_max =", y_max)
+
+        d_d.setspace('X', svgrange=(100,450), categorical=test_cases_picks_on)
+        d_d.setspace('Y', svgrange=(290,500), datarange=(0,y_max))
+
+        d_d.axis('X', loc='bottom-18', axisline=False, stubrotate=50)
+        d_d.axis('Y', grid=True)
+        d_d.plotlabels(ylabel='execution time [ms]', ylabelpos=-70)
+
+        # draw the column bars:
+        for i, item in enumerate(test_cases_picks_on):
+            d_d.bar(x=item, y=test_values_picks_on[i], color='#88AA88', width=16)
+
+        # title:
+        d_d.settext(ptsize=20, adjust=(-45,0))
+        d_d.plotlabels(titlepos=+20, title= diagram_title)
+        d_d.settext(restore=True)
 
         return d_d.svgresult()
 
