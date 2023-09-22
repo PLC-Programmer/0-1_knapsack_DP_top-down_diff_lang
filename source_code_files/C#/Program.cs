05ec2d9b8ff203290967053f2a5ba3265bf55b02
R.Sackmann <iec61131@my.mail.de>
2023-09-23 00:59:14 +0200

    Update Program.cs

--- source_code_files/C#/Program.cs
+++ source_code_files/C#/Program.cs
@@ -1,48 +1,51 @@
 // Program.cs
 //
-// Robert Sackmann, 2023-09-15
+// Robert Sackmann, 2023-09-23
 //
 //
 // test:
 // environment: $ uname -a --> Linux ... 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
+//              $ dotnet --info --> 7.0.111 [/usr/lib/dotnet/sdk],
+//                                  Microsoft.AspNetCore.App 7.0.11 [/usr/lib/dotnet/shared/Microsoft.AspNetCore.App]
+//                                  Microsoft.NETCore.App 7.0.11 [/usr/lib/dotnet/shared/Microsoft.NETCore.App]
+//
 //
 // ini:   $ ./C#/dotnet new console -n dp_knapsack_top-down --use-program-main
 //
 // build a development candidate:
 // build:   ./C#/dp_knapsack_top-down $ dotnet build
-// run:     ./C#/dp_knapsack_top-down $ ./bin/Debug/net7.0/dp_knapsack_top-down [no_picks, nopicks, picks_off, picksoff]
+// run:     ./C#/dp_knapsack_top-down $ ./bin/Debug/net7.0/dp_knapsack_top-down [no_picks, nopicks, picks_off, picksoff] [no_timer, notimer, timer_off, timeroff]
 //
 // build a release candidate:
 // build:   ./C#/dp_knapsack_top-down $ dotnet publish -c Release -r linux-x64 --no-self-contained
-//     dp_knapsack_top-down -> /home/booser/scripts/knapsack_in_different_lanuages/C#/dp_knapsack_top-down/bin/Release/net7.0/linux-x64/dp_knapsack_top-down.dll
-//     dp_knapsack_top-down -> /home/booser/scripts/knapsack_in_different_lanuages/C#/dp_knapsack_top-down/bin/Release/net7.0/linux-x64/publish/
-// run:     ./C#/dp_knapsack_top-down $ ./bin/Release/net7.0/linux-x64/dp_knapsack_top-down [no_picks, nopicks, picks_off, picksoff]
+// run:     ./C#/dp_knapsack_top-down $ ./bin/Release/net7.0/linux-x64/dp_knapsack_top-down [no_picks, nopicks, picks_off, picksoff] [no_timer, notimer, timer_off, timeroff]
 //
 //
 // to do:
 //   -
 //
 //
-// 2023-09-15: execution times with picks table activated:
-//     01_WEIGHTS4.in                     8ms
-//     02_WEIGHTS24_Kreher&Stinson.in   184ms
-//     03_WEIGHTS100_Xu_Xu_et_al.in    9098ms
-//     04_WEIGHTS_TODD_16.in             18ms
-//     04_WEIGHTS_TODD_17.in             38ms
-//     05_WEIGHTS_TODD_18.in             69ms
+// 2023-09-23: execution times (Total number of CPU-milliseconds that the process spent in user mode) with picks table activated:
+//   $ bash shell script "exe_times_statistics_for_multiple_test_cases" (10x) with test case files (*.in) located in directory: ./test_cases
+//     01_WEIGHTS4.in                    34ms
+//     02_WEIGHTS24_Kreher&Stinson.in   172ms
+//     03_WEIGHTS100_Xu_Xu_et_al.in    8989ms
+//     04_WEIGHTS_TODD_16.in             35ms
+//     04_WEIGHTS_TODD_17.in             35ms
+//     05_WEIGHTS_TODD_18.in             42ms
 //     06_WEIGHTS_TODD_19.in             ----  <Array dimensions exceeded supported range.>
 //     06_WEIGHTS_TODD_20.in             ----  <Array dimensions exceeded supported range.>
 //     7.in                              ----  <stopped manually after many minutes>
 //
-// 2023-09-15: execution times without picks table activated:
-//     01_WEIGHTS4.in                     9ms
-//     02_WEIGHTS24_Kreher&Stinson.in    57ms
-//     03_WEIGHTS100_Xu_Xu_et_al.in    8848ms
-//     04_WEIGHTS_TODD_16.in              1ms
-//     04_WEIGHTS_TODD_17.in              2ms
-//     05_WEIGHTS_TODD_18.in              2ms
-//     06_WEIGHTS_TODD_19.in              2ms
-//     06_WEIGHTS_TODD_20.in              4ms
+// 2023-09-21: same, but without picks table activated:
+//     01_WEIGHTS4.in                    28ms
+//     02_WEIGHTS24_Kreher&Stinson.in    88ms
+//     03_WEIGHTS100_Xu_Xu_et_al.in    8529ms
+//     04_WEIGHTS_TODD_16.in             32ms
+//     04_WEIGHTS_TODD_17.in             34ms
+//     05_WEIGHTS_TODD_18.in             31ms
+//     06_WEIGHTS_TODD_19.in             36ms
+//     06_WEIGHTS_TODD_20.in             37ms
 //     7.in                              ----  <see above: not tested>
 
 
@@ -77,11 +80,19 @@ class Program
 
 
         bool picks_on = true;  // default: with picks table activated
+        bool exe_timer_on = true;  // default: with internal execution timer activated
 
         if (args.Length > 0) {
             if (args[0] == "no_picks" || args[0] == "nopicks" || args[0] == "picks_off" || args[0] == "picksoff" )
             {
                 picks_on = false;  // no picks table to be activated
+            }
+
+            if (args.Length > 1) {
+                if (args[1] == "no_timer" || args[1] == "notimer" || args[1] == "timer_off" || args[1] == "timeroff" )
+                {
+                    exe_timer_on = false;  // no internal execution timer to be activated
+                }
             }
         }
 
@@ -166,38 +177,63 @@ class Program
             //
             // Dynamic Programming top-down (recursive) procedure:
 
-            ProcessStartInfo startInfo = new ProcessStartInfo()
-            {
-                FileName = "/bin/bash",
-                Arguments = "-c \"date +'%s%3N'\"",  // milliseconds since 1970-01-01 00:00:00 UTC
-                RedirectStandardOutput = true
-            };
-            Process proc = new Process() { StartInfo = startInfo, };
-
             if (picks_on is true) {
-                proc.Start();
-                var t1 = proc.StandardOutput.ReadToEnd();
+                if (exe_timer_on is true) {
+                    ProcessStartInfo startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "/bin/bash",
+                        Arguments = "-c \"date +'%s%3N'\"",  // milliseconds since 1970-01-01 00:00:00 UTC
+                        RedirectStandardOutput = true
+                    };
 
-                knapsack_recursive ks_rec = new knapsack_recursive(w, v, max_wt);  // function in another source file
-                result = knapsack_recursive.result;
+                    Process proc = new Process() { StartInfo = startInfo, };
+                    proc.Start();
+                    var t1 = proc.StandardOutput.ReadToEnd();
 
-                proc.Start();
-                var t2 = proc.StandardOutput.ReadToEnd();
-                stopped_time = Convert.ToInt64(t2) - Convert.ToInt64(t1);  // in milliseconds
+                    knapsack_recursive ks_rec = new knapsack_recursive(w, v, max_wt);  // function in another source file
+                    result = knapsack_recursive.result;
+
+                    proc.Start();
+                    var t2 = proc.StandardOutput.ReadToEnd();
+                    stopped_time = Convert.ToInt64(t2) - Convert.ToInt64(t1);  // in milliseconds
+
+                    Console.WriteLine("\n  Dynamic programming top-down (recursive) was completed with answer = " + result + " and execution time = " + stopped_time + "ms");
+                }
+                else {
+                    knapsack_recursive ks_rec = new knapsack_recursive(w, v, max_wt);  // function in another source file
+                    result = knapsack_recursive.result;
+                    Console.WriteLine("\n  Dynamic programming top-down (recursive) was completed with answer = " + result);
+                }
             }
             else {
-                proc.Start();
-                var t1 = proc.StandardOutput.ReadToEnd();
+                if (exe_timer_on is true) {
+                    ProcessStartInfo startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "/bin/bash",
+                        Arguments = "-c \"date +'%s%3N'\"",  // milliseconds since 1970-01-01 00:00:00 UTC
+                        RedirectStandardOutput = true
+                    };
 
-                knapsack_recursive_no_picks ks_rec = new knapsack_recursive_no_picks(w, v, max_wt);  // function in another source file
-                result = knapsack_recursive_no_picks.result;
+                    Process proc = new Process() { StartInfo = startInfo, };
+                    proc.Start();
+                    var t1 = proc.StandardOutput.ReadToEnd();
 
-                proc.Start();
-                var t2 = proc.StandardOutput.ReadToEnd();
-                stopped_time = Convert.ToInt64(t2) - Convert.ToInt64(t1);  // in milliseconds
+                    knapsack_recursive_no_picks ks_rec = new knapsack_recursive_no_picks(w, v, max_wt);  // function in another source file
+                    result = knapsack_recursive_no_picks.result;
+
+                    proc.Start();
+                    var t2 = proc.StandardOutput.ReadToEnd();
+                    stopped_time = Convert.ToInt64(t2) - Convert.ToInt64(t1);  // in milliseconds
+
+                    Console.WriteLine("\n  Dynamic programming top-down (recursive) was completed with answer = " + result + " and execution time = " + stopped_time + "ms");
+                }
+                else {
+                    knapsack_recursive_no_picks ks_rec = new knapsack_recursive_no_picks(w, v, max_wt);  // function in another source file
+                    result = knapsack_recursive_no_picks.result;
+                    Console.WriteLine("\n  Dynamic programming top-down (recursive) was completed with answer = " + result);
+                }
             }
 
-            Console.WriteLine("\n  Dynamic programming top-down (recursive) was completed with answer = " + result + " and execution time = " + stopped_time + "ms");
             Console.WriteLine("  The expected maximum value of the optimal knapsack is " + optimal_profit);
 
             //
