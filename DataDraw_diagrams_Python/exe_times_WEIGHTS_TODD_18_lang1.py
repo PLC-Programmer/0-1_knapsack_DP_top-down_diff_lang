d8881db6c35033f8875401a8027253a99ddd279a
R.Sackmann <iec61131@my.mail.de>
2023-09-25 00:08:35 +0200

    Update exe_times_WEIGHTS_TODD_18_lang1.py
    updating execution times from mass testing

--- DataDraw_diagrams_Python/exe_times_WEIGHTS_TODD_18_lang1.py
+++ DataDraw_diagrams_Python/exe_times_WEIGHTS_TODD_18_lang1.py
@@ -1,21 +1,28 @@
 """
-Knapsack: dynamic programming: top-down (recursive):
-execution times [ms] for the 05_WEIGHTS_TODD_18.in test case
-for the first group of programming languages: Python, C++, C#, Rust
-nan (not a number): not enough memory or timeout exceeded
+rev.2023-09-25
+Knapsack: dynamic programming: top-down (recursive)
+Execution times [ms] ("Total number of CPU-seconds
+  that the process spent in user mode") for the
+  05_WEIGHTS_TODD_18.in test case for the first group
+  of programming languages: Python, C++, C#, Rust
+- nan (not a number): not enough memory or timeout exceeded
+Using the bash script with the Linux time command:
+  - exe_times_statistics_for_multiple_test_cases
+  - 10 rounds each test case
+  - internal timer off: [no_timer, notimer, timer_off, timeroff]
 """
 
 picks_on = [
     ('Python', float('nan')),
-    ('C++', 1111),
-    ('C#', 69),
-    ('Rust', 87)
+    ('C++', 181),
+    ('C#', 42),
+    ('Rust', 3)
 ]
 
 
 picks_off = [
     ('Python', 58),
-    ('C++', 0.3),
-    ('C#', 2),
+    ('C++', 0),
+    ('C#', 31),
     ('Rust', 0)
 ]
