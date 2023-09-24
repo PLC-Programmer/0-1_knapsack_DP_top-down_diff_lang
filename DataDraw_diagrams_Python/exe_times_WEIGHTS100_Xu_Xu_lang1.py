8e75cf00611c19eccd790620f2282fd454eab83f
R.Sackmann <iec61131@my.mail.de>
2023-09-24 23:50:38 +0200

    Update exe_times_WEIGHTS100_Xu_Xu_lang1.py
    updating execution times from mass testing

--- DataDraw_diagrams_Python/exe_times_WEIGHTS100_Xu_Xu_lang1.py
+++ DataDraw_diagrams_Python/exe_times_WEIGHTS100_Xu_Xu_lang1.py
@@ -1,21 +1,28 @@
 """
-Knapsack: dynamic programming: top-down (recursive):
-execution times [ms] for the 03_WEIGHTS100_Xu_Xu_et_al.in test case
-for the first group of programming languages: Python, C++, C#, Rust
-nan (not a number): not enough memory or timeout exceeded
+rev.2023-09-24
+Knapsack: dynamic programming: top-down (recursive)
+Execution times [ms] ("Total number of CPU-seconds
+  that the process spent in user mode") for the
+  03_WEIGHTS100_Xu_Xu_et_al.in test case for the first group
+  of programming languages: Python, C++, C#, Rust
+- nan (not a number): not enough memory or timeout exceeded
+Using the bash script with the Linux time command:
+  - exe_times_statistics_for_multiple_test_cases
+  - 10 rounds each test case
+  - internal timer off: [no_timer, notimer, timer_off, timeroff]
 """
 
 picks_on = [
-    ('Python', 461218),
-    ('C++', 3511),
-    ('C#', 9098),
-    ('Rust', 8206)
+    ('Python', 462407),
+    ('C++', 3583),
+    ('C#', 8989),
+    ('Rust', 8143)
 ]
 
 
 picks_off = [
-    ('Python', 406398),
-    ('C++', 2900),
-    ('C#', 8848),
-    ('Rust', 6940)
+    ('Python', 403686),
+    ('C++', 2896),
+    ('C#', 8529),
+    ('Rust', 6907)
 ]
