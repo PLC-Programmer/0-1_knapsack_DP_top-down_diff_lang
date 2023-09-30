0fa9bd20e3860b33112760f85a0f18a73085211f
R.Sackmann <iec61131@my.mail.de>
2023-09-30 13:11:23 +0200

    Add files via upload

---
+++ DataDraw_diagrams_Python/exe_times_02_cpp_array_vs_vector.py
@@ -0,0 +1,30 @@
+"""
+rev.2023-09-30
+Knapsack: dynamic programming: top-down (recursive)
+C++ program (-O3): execution times [ms]
+  ("Total number of CPU-seconds that the process spent in user mode")
+  >>> alternatively, picks table done with array instead of vector data type <<<
+- nan (not a number): not enough memory or timeout exceeded
+Using the bash script with the Linux time command:
+  - exe_times_statistics_for_multiple_test_cases
+  - 10 rounds each test case
+  - internal timer off: [no_timer, notimer, timer_off, timeroff]
+"""
+
+alt1 = [
+    ('01_WEIGHTS4.in', 0),
+    ('02_WEIGHTS24_Kreher_Stinson.in', 109),
+    ('03_WEIGHTS100_Xu_Xu_et_al.in', 3583),
+    ('04_WEIGHTS_TODD_16.in', 30),
+    ('04_WEIGHTS_TODD_17.in', 84),
+    ('05_WEIGHTS_TODD_18.in', 181),
+]
+
+alt2 = [
+    ('01_WEIGHTS4.in', 0),
+    ('02_WEIGHTS24_Kreher_Stinson.in', 82),
+    ('03_WEIGHTS100_Xu_Xu_et_al.in', 3231),
+    ('04_WEIGHTS_TODD_16.in', 0),
+    ('04_WEIGHTS_TODD_17.in', 2),
+    ('05_WEIGHTS_TODD_18.in', 5),
+]
---
+++ DataDraw_diagrams_Python/exe_times_02_cpp_array_vs_vector_TODD.py
@@ -0,0 +1,26 @@
+"""
+rev.2023-09-30
+Knapsack: dynamic programming: top-down (recursive)
+C++ program (-O3): execution times [ms]
+  ("Total number of CPU-seconds that the process spent in user mode")
+  >>> alternatively, picks table done with array instead of vector data type <<<
+- nan (not a number): not enough memory or timeout exceeded
+Using the bash script with the Linux time command:
+  - exe_times_statistics_for_multiple_test_cases
+  - 10 rounds each test case
+  - internal timer off: [no_timer, notimer, timer_off, timeroff]
+"""
+
+alt1 = [
+    ('02_WEIGHTS24_Kreher_Stinson.in', 109),
+    ('04_WEIGHTS_TODD_16.in', 30),
+    ('04_WEIGHTS_TODD_17.in', 84),
+    ('05_WEIGHTS_TODD_18.in', 181),
+]
+
+alt2 = [
+    ('02_WEIGHTS24_Kreher_Stinson.in', 82),
+    ('04_WEIGHTS_TODD_16.in', 0),
+    ('04_WEIGHTS_TODD_17.in', 2),
+    ('05_WEIGHTS_TODD_18.in', 5),
+]
